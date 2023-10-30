dd1f70b940593fb314ef40d483c8f693f61299b3
JCRoooD <juancarlos-99@live.com>
2023-10-30 14:16:23 -0400

    completed task #3 class Base and empty innit files

--- models/base_model.py
+++ models/base_model.py
@@ -4,19 +4,28 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """This is the BaseModel class"""
-    
-    def __init__(self, *args, **kwargs):
+
+    def __init__(self):
         """Initialization of the class"""
-    
+        self.id = uuid.uuid4()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns the string representation of the class""" 
-
+        return F"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
+    
     def save(self):
         """Updates the public instance attribute updated_at"""
-
+        self.update_at = datetime.now()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__"""
+        class_dict = dict(self.__dict__)
+        class_dict["__class__"] = self.__class__.__name__
+        class_dict["created_at"] = self.created_at.isoformat()
+        class_dict["updated_at"] = self.updated_at.isoformat()
+        return class_dict
