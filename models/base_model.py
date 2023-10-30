ce9936978175c8d72bfd06c254158c746b3e0e24
JCRoooD <juancarlos-99@live.com>
2023-10-30 14:29:14 -0400

    fixing

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel:
 
     def __init__(self):
         """Initialization of the class"""
-        self.id = (uuid.uuid4{})
+        self.id = uuid.uuid4()
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
