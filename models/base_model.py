f991f8dbed1230be43286ddf8a2211ba4afe094c
JCRoooD <juancarlos-99@live.com>
2023-10-31 14:25:55 -0400

    fixed typo in basemodel and file storage now runs

--- models/base_model.py
+++ models/base_model.py
@@ -28,7 +28,7 @@ class BaseModel:
 
     def save(self):
         """Updates the public instance attribute updated_at"""
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
 
