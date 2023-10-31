8361dcbe5fee1827b22194d3bcab2c65dc8c388d
JCRoooD <juancarlos-99@live.com>
2023-10-31 15:03:58 -0400

    command interpreter

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Console module"""
+
+import cmd
+from models.base_model import BaseModel
+from models import storage
+
+
+class HBNBCommand(cmd.Cmd):
+    """HBNCB command class"""
+
+    prompt = ' (hbnb) '
+
+    def do_quit(self, arg):
+        """quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+
+    def empty(self, arg):
+        """empty line"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
