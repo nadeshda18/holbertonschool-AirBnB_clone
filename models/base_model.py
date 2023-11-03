6c1fe670af2796abbac515205b1b1ebc6b5c8a58
JCRoooD <juancarlos-99@live.com>
2023-11-03 13:53:37 -0400

    pycodestyle fix

--- models/base_model.py
+++ models/base_model.py
@@ -13,14 +13,14 @@ class BaseModel:
         """Initialization of the class"""
         if kwargs:
             for key, value in kwargs.items():
-                # Convert string to datetime object if key is "created_at" or "updated_at"
+                # Convert ISO format strings to datetime objects
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 # Set attribute if key is not "__class__"
                 if key != "__class__":
                     setattr(self, key, value)
         else:
-            # Set id, created_at, and updated_at attributes if no kwargs are passed
+            # Set id, created_at, and updated_at attributes
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
